43e45f158ac3c53bf295098847757b598ca0d3e0
Atharva <atharvamainwork@gmail.com>
2024-09-20 20:19:58 +0530

    made routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// Import your components
+import AddAssignmentForm from "./components/AddAssignmentForm";
+import AssignmentSubmission from "./components/AssignmentSubmission";
+import BorrowedBooksPage from "./components/BorrowedBooksPage";
+import Calendar from "./components/Calendar";
+import CodingPlatform from "./components/CodingPlatform";
+import ExamScheduler from "./components/ExamScheduler";
+import Login from "./components/Login";
+import Notes from "./components/Notes";
+import PlagiarismChecker from "./components/PlagiarismChecker";
+import Skills from "./components/Skills";
+import TeacherAssignmentView from "./components/TeacherAssignmentView";
 import Test from "./components/test";
+import { BookList } from "./components/BookList";
 
 function App() {
   return (
-    <>
-      <div className="">
-        <Test />
+    <Router>
+      <div>
+        <Routes>
+          <Route path ="/" element={<Login />} />
+          <Route path ="/add-assignment" element={<AddAssignmentForm />} />
+          <Route path ="/assignment-submission" element={<AssignmentSubmission />} />
+          <Route path="/book-list" element={<BookList />} />
+          <Route path="/borrowed-books" element={<BorrowedBooksPage />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/coding-platform" element={<CodingPlatform />} />
+          <Route path="/exam-scheduler" element={<ExamScheduler />} />
+          <Route path="/notes" element={<Notes />} />
+          <Route path="/plagiarism-checker" element={<PlagiarismChecker />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/teacher-assignment-view" element={<TeacherAssignmentView />} />
+          <Route path="/test" element={<Test />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
