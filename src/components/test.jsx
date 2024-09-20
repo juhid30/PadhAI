4121dc35196fe6676c65a26e2625e3618cef8950
kavyachetwani <kavya422chetwani@gmail.com>
2024-09-20 12:42:51 +0530

    aworfn

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Calendar from "./Calendar";
 import Skills from "./Skills";
+import Notes from "./Notes";
 
 const Test = () => {
-  return <Skills />;
+  return <Notes />;
 };
 
 export default Test;
