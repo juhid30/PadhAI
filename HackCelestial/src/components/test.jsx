ee3fd5e8b215e86b5a09b4662c3cddff0a51556b
jashrashne <jash.rashne@gmail.com>
2024-09-20 11:48:04 +0530

    tailwind config

--- HackCelestial/src/components/test.jsx
+++ HackCelestial/src/components/test.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Calendar from "./Calendar";
+import Skills from "./Skills";
 
 const Test = () => {
-  return <Calendar />;
+  return <Skills />;
 };
 
 export default Test;
