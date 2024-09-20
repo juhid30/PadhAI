2d49dea0be1225792de982fe540c5519a584e828
jashrashne <jash.rashne@gmail.com>
2024-09-20 17:20:18 +0530

    commit

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Calendar from "./Calendar";
 import Skills from "./Skills";
 import Notes from "./Notes";
+import CodingPlatform from "./CodingPlatform";
+import PlagiarismChecker from "./PlagiarismChecker";
 
 const Test = () => {
-  return <Notes />;
+  return <PlagiarismChecker />;
 };
 
 export default Test;
